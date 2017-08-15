43851277304bb8fd3a77a0187324824fedcd5abe
Nate <n8bdoesemailvia@gmail.com>
2017-08-15 10:27:18 -0600

    READ.me update

---
+++ app.js
@@ -0,0 +1,23 @@
+// $(document).ready(function(){
+//
+//   $("h1").hide()
+//
+//   $("#Score").change(function(){
+//
+//     var $mon = $("#Score").val();
+//
+//     $.get ("http://pokeapi.co/api/v2/pokemon/" + $mon + "/")
+//
+//     .then function(data){
+//
+//       $("h1").show();
+//
+//       var pokeName = data.name
+//
+//       $("h1").html(pokeName)
+//
+//     });
+//   });
+//
+//
+// });
